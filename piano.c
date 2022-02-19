407d87ce0bcac87ff09f2998e4dbb0f9c8d3f395
Lolo <mjmorales.mcv@gmail.com>
2022-02-19 14:48:53 +0100

    Add files via upload

--- piano.c
+++ piano.c
@@ -1,26 +1,26 @@
 #include <windows.h>
 #include <conio.h>
 
-#define MAX_LONG 30*1000 // 30 segundos en milisegundos
-#define LOWEST_NOTE 100
+#define MAX_LONG 1*1000 // 30 segundos en milisegundos
+#define LOWEST_NOTE 5
 #define ESPACE ' '
 #define ESCAPE 27
 
 int main(){
     char martillo = 0;
     char nota = 0;
-    while(true){
+    while(1){
         int freq = LOWEST_NOTE;
         if(kbhit()){
             switch(nota = getch()){
                 case ESPACE:
                     martillo = martillo > 0 ? 0 : 1;
-                    breaK;
+                    break;
                 case ESCAPE:
                     exit(0);
                 default:
                     freq*=nota;
-                    beep(freq,martillo?MAX_LONG/2:MAX_LONG);
+                    Beep(freq,martillo?150:350);
                     break;
             }
         }
